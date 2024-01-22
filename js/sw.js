fa0b075f2958180ab490c897ac8e9f6e5e7ae266
ShHawk <alexandreuzan9@gmail.com>
2024-01-23 00:46:25 +0100

    ADD sw.js Service Worker

---
+++ js/sw.js
@@ -0,0 +1,20 @@
+
+self.addEventListener('install', event => {
+    console.log('Service Worker installing.');
+});
+
+self.addEventListener('activate', event => {
+    console.log('Service Worker activated.');
+});
+
+self.addEventListener('push', event => {
+    const data = event.data.json(); 
+    const title = data.title || 'Notification';
+    const options = {
+        body: data.body,
+        icon: 'icon.png', 
+        badge: 'badge-icon.png'
+    };
+
+    event.waitUntil(self.registration.showNotification(title, options));
+});
