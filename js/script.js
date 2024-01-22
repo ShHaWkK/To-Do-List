c34a4f264cee57c3ac2c42d98597bb3c717a1fa0
ShHawk <alexandreuzan9@gmail.com>
2024-01-23 00:47:39 +0100

    VAPID Key Conversion and Notification (loading)

--- js/script.js
+++ js/script.js
@@ -100,3 +100,50 @@ function filterTasks() {
         }
     });
 }
+function askPermission() {
+    return new Promise(function(resolve, reject) {
+        const permissionResult = Notification.requestPermission(function(result) {
+            resolve(result);
+        });
+
+        if (permissionResult) {
+            permissionResult.then(resolve, reject);
+        }
+    })
+    .then(function(permissionResult) {
+        if (permissionResult !== 'granted') {
+            throw new Error('Permission non accordée.');
+        }
+    });
+}
+
+function subscribeUserToPush() {
+    return navigator.serviceWorker.register('sw.js')
+        .then(function(registration) {
+            const subscribeOptions = {
+                userVisibleOnly: true,
+                applicationServerKey: urlBase64ToUint8Array('VOTRE_CLÉ_PUBLIC_VAPID')
+            };
+
+            return registration.pushManager.subscribe(subscribeOptions);
+        })
+        .then(function(pushSubscription) {
+            console.log('Reçu PushSubscription: ', JSON.stringify(pushSubscription));
+            return pushSubscription;
+        });
+}
+
+function urlBase64ToUint8Array(base64String) {
+    const padding = '='.repeat((4 - base64String.length % 4) % 4);
+    const base64 = (base64String + padding)
+        .replace(/\-/g, '+')
+        .replace(/_/g, '/');
+    
+    const rawData = window.atob(base64);
+    const outputArray = new Uint8Array(rawData.length);
+
+    for (let i = 0; i < rawData.length; ++i) {
+        outputArray[i] = rawData.charCodeAt(i);
+    }
+    return outputArray;
+}
