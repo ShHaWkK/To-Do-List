1470769f8c769a5c374c1d043b2f86543f1a52f1
ShHaWkK <alexandreuzan9@gmail.com>
2024-01-29 00:15:24 +0100

    Merge import & export on script.js

--- js/script.js
+++ js/script.js
@@ -231,4 +231,29 @@ function NotificationPush() {
         });
 }
 
+function importTasks(event) {
+    const file = event.target.files[0];
+
+    if (file) {
+        const reader = new FileReader();
+        reader.onload = function(e) {
+            const tasks = JSON.parse(e.target.result);
+            localStorage.setItem('tasks', JSON.stringify(tasks));
+            loadTasks(); // Recharger la liste de tâches
+        };
+        reader.readAsText(file);
+    }
+}
+document.getElementById('import').addEventListener('change', importTasks);
+
+function exportTasks() {
+    const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+    const tasksJson = JSON.stringify(tasks, null, 2);
+    const blob = new Blob([tasksJson], {type : 'application/json'});
+
+    const a = document.createElement('a');
+    a.href = URL.createObjectURL(blob);
+    a.download = 'tasks.json';
+    a.click();
+}
 
